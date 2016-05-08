22e92cd6d9a5cb64d4dafdafa3bd5a71b2ec66f0
6thSence <d.pushkarskaya.pr@gmail.com>
2016-05-08 18:00:59 +0600

    postcss-boilerplate  => my plugin postcss-cler-fix

---
+++ README.md
@@ -0,0 +1,27 @@
+# PostCSS Clearfix [![Build Status][ci-img]][ci]
+
+[PostCSS] plugin when create clearfix from one world.
+
+[PostCSS]: https://github.com/postcss/postcss
+[ci-img]:  https://travis-ci.org/6thSence/postcss-clearfix.svg
+[ci]:      https://travis-ci.org/6thSence/postcss-clearfix
+
+```css
+.foo {
+    /* Input example */
+}
+```
+
+```css
+.foo {
+  /* Output example */
+}
+```
+
+## Usage
+
+```js
+postcss([ require('postcss-clearfix') ])
+```
+
+See [PostCSS] docs for examples for your environment.
---
+++ index.js
@@ -0,0 +1,50 @@
+var postcss = require('postcss');
+
+module.exports = postcss.plugin('postcss-clearfix', function (opts) {
+    opts = opts || {};
+
+    return function (css, result) {
+
+        css.walkDecls(decl => {
+            if (decl.prop === 'clearfix') {
+                const parent = decl.parent;
+                const rule = postcss.rule({ selector: decl.parent.selector + ':after' });
+                rule.append({
+                        type: 'decl',
+                        prop: 'clear',
+                        value: 'both',
+                        source: parent.source,
+                        raws: {
+                            before: "\n\t",
+                            between: ": "
+                        }
+                    },
+                    {
+                        type: 'decl',
+                        prop: 'display',
+                        value: 'block',
+                        source: parent.source,
+                        raws: {
+                            before: "\n\t",
+                            between: ": "
+                        }
+                    },
+                    {
+                        type: 'decl',
+                        prop: 'content',
+                        value: '""',
+                        source: parent.source,
+                        raws: {
+                            before: "\n\t",
+                            between: ": "
+                        }
+                    });
+
+                decl.root().insertAfter(decl.parent, rule)
+                rule.source = decl.parent.source;
+                decl.remove();
+            }
+    });
+
+    };
+});
